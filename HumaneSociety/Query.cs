7f7de179113263f8f4f5038cc78d3b379d441922
jayr28 <poserio.rodolfo@gmail.com>
2018-10-17 10:24:50 -0500

    added a collaborator

---
+++ HumaneSociety/Query.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    public static class Query
+    {
+       
+        internal static void RunEmployeeQueries(Employee employee, string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Room GetRoom(int animalId)
+        {
+            throw new NotImplementedException();
+        }
+
+      
+       
+    }
+}
